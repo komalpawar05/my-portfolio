7f1a0aa7be9b4a20d3a93db1f0db1231e8a38518
komalpawar05 <komalpawar05051997@gmail.com>
2026-09-10 17:18:32 +0530

    Refactor: Remove outdated resume PDF from assets

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import Profile_Image from "../assets/profile_photo.png";
 import { FaGithub, FaLinkedin, FaDownload } from "react-icons/fa";
-import Resume from "../assets/komal_rusume.pdf";
+import Resume from "../assets/komal_resume05.pdf";
 
 const Hero = () => {
   return (
